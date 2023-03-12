33695af53d6bd648d987c84b8cf1656c1fbbdd66
GemWinnnn <126636338+GemWinnnn@users.noreply.github.com>
2023-03-12 12:39:35 +0800

    Update Activity_2.py

--- Activity_2.py
+++ Activity_2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
+
 
 two_d_arr = np.array([[1,0,1], [0,1,0], [1,0,1]])
 
