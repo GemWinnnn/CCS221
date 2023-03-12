af200cd5ac0862acbd6c498dec34a548231c9fc9
GemWinnnn <126636338+GemWinnnn@users.noreply.github.com>
2023-03-12 13:36:21 +0800

    Update Activity_2.py

--- Activity_2.py
+++ Activity_2.py
@@ -19,9 +19,10 @@ def main():
     st.write(two_d_arr)
 
     for i in range(3):
-        x_val = st.number_input("X coordinate (row 0-2):", min_value=0, max_value=2)
-        y_val = st.number_input("Y coordinate (column 0-2):", min_value=0, max_value=2)
-        c_val = st.number_input("Color Value (1-50):", min_value=1, max_value=50)
+        
+        x_val = st.number_input("X coordinate (row 0-2):", key=f"x_{i}", min_value=0, max_value=2)
+        y_val = st.number_input("Y coordinate (column 0-2):", key=f"y_{i}", min_value=0, max_value=2)
+        c_val = st.number_input("Color Value (1-50):", key=f"c_{i}", min_value=1, max_value=50)
 
         change(two_d_arr, x_val, y_val, c_val)
 
