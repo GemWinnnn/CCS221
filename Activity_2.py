4a492f08055d0716384f7586305a4f1653bec234
GemWinnnn <126636338+GemWinnnn@users.noreply.github.com>
2023-03-12 12:55:12 +0800

    Update Activity_2.py

--- Activity_2.py
+++ Activity_2.py
@@ -2,17 +2,16 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
 two_d_arr = np.array([[1,0,1], [0,1,0], [1,0,1]])
 
 def change(x, y, color):
     global two_d_arr
     two_d_arr[x][y] = color
-    img = plt.imshow(two_d_arr, interpolation='none', cmap='plasma')
+    fig, ax = plt.subplots()
+    img = ax.imshow(two_d_arr, interpolation='none', cmap='plasma')
     img.set_clim([0,50])
     plt.colorbar()
-    st.pyplot()
+    return fig
 
 def main():
     st.title("2D Array Editor")
@@ -22,7 +21,9 @@ def main():
         y_val = st.number_input("Y coordinate (column 0-2):", key=f"y_{i}", min_value=0, max_value=2)
         c_val = st.number_input("Color Value (1-50):", key=f"c_{i}", min_value=1, max_value=50)
 
-        change(x_val, y_val, c_val)
+        fig = change(x_val, y_val, c_val)
+        st.pyplot(fig)
+
         st.write("Updated Array:")
         st.write(two_d_arr)
 
