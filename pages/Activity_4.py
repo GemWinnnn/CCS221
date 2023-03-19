67cbb39f80bd25075952f663153dbc20136127af
GemWinnnn <gemwin.canete@wvsu.edu.ph>
2023-03-19 12:03:48 +0800

    update

--- pages/Activity_4.py
+++ pages/Activity_4.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial import Delaunay
 import tensorflow as tf
 import streamlit as st
+
 
 # Add Streamlit components
 st.title("3D Object Translator")
