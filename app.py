4c688ebeac1c92c513938cd5090f30c67d27015a
GemWinnnn <gemwin.canete@wvsu.edu.ph>
2023-03-19 18:02:56 +0800

    added image

--- app.py
+++ app.py
@@ -6,3 +6,4 @@ import pandas as pd
 
 st.title("Midterm Test")
 st.write("submitted by: Group 9 ")
+st.image("https://assets.entrepreneur.com/content/3x2/2000/1649279368-Ent-2022Python.jpeg?crop=4:3")
