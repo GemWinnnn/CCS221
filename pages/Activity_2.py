e7664ff3445598e6c1759937505f94f06c34bc69
GemWinnnn <gemwin.canete@wvsu.edu.ph>
2023-03-19 17:24:52 +0800

    updated

--- pages/Activity_2.py
+++ pages/Activity_2.py
@@ -15,12 +15,6 @@ def main():
     st.title("2D Array Editor")
 
     two_d_arr = np.array([[1,0,1], [0,1,0], [1,0,1]])
-    st.write("Original Array:")
-    st.write(two_d_arr)
-
-    st.write("Updated Array:")
-    st.write(two_d_arr)
-
     x_val = st.number_input("X coordinate (row 0-2):", key="x", min_value=0, max_value=2)
     y_val = st.number_input("Y coordinate (column 0-2):", key="y", min_value=0, max_value=2)
     c_val = st.number_input("Color Value (1-50):", key="c", min_value=1, max_value=50)
