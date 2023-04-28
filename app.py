7c01f1fb19b371f4c8cda662b498aa9fdf24b5bc
GemWinnnn <126636338+GemWinnnn@users.noreply.github.com>
2023-04-28 10:02:53 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np 
+import matplotlib as plt
+from io import StringIO
+import pandas as pd
+
+st.title("Midterm Test")
+st.write("submitted by: Group 9 ")
+st.image("https://media.tenor.com/gj6ZITvG01QAAAAC/code-review-silicon-valley.gif")
